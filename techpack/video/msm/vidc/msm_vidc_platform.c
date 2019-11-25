e1977d2d9259b4f3ea36adeda1c6754608e85879
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-11-25 13:33:54 +0100

    techpack: video: platform: Fix sdm845 properties
    Add fw cycles and fw-vpp-cycles for correct calculation of HFR
    and VPP usecases.
    
    Also, fix the power collapse delay and hw response timeout for
    older firmwares and set the right maximum secure instances.

--- techpack/video/msm/vidc/msm_vidc_platform.c
+++ techpack/video/msm/vidc/msm_vidc_platform.c
@@ -1706,7 +1706,7 @@ static struct msm_vidc_common_data sdm845_common_data[] = {
 	},
 	{
 		.key = "qcom,max-secure-instances",
-		.value = 5,
+		.value = 2,
 	},
 	{
 		.key = "qcom,max-hw-load",
@@ -1730,11 +1730,11 @@ static struct msm_vidc_common_data sdm845_common_data[] = {
 	},
 	{
 		.key = "qcom,power-collapse-delay",
-		.value = 500,
+		.value = 1500,
 	},
 	{
 		.key = "qcom,hw-resp-timeout",
-		.value = 250,
+		.value = 1000,
 	},
 	{
 		.key = "qcom,debug-timeout",
@@ -1743,6 +1743,14 @@ static struct msm_vidc_common_data sdm845_common_data[] = {
 	{
 		.key = "qcom,dcvs",
 		.value = 1,
+	},
+	{
+		.key = "qcom,fw-cycles",
+		.value = 760000,
+	},
+	{
+		.key = "qcom,fw-vpp-cycles",
+		.value = 166667,
 	},
 };
 
