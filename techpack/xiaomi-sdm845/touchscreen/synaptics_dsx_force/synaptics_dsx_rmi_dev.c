843c481366ab2f914ff203d30424eaa058839c2a
tanz3 <3222839916@qq.com>
2023-02-25 15:18:18 +0800

    input: touchscreen: Switch to xiaomi new drm
    Signed-off-by: tanz3 <3222839916@qq.com>

--- techpack/xiaomi-sdm845/touchscreen/synaptics_dsx_force/synaptics_dsx_rmi_dev.c
+++ techpack/xiaomi-sdm845/touchscreen/synaptics_dsx_force/synaptics_dsx_rmi_dev.c
@@ -39,6 +39,7 @@
 #include <linux/input.h>
 #include <linux/sched/signal.h>
 #include <linux/sched.h>
+#include <linux/sched/signal.h>
 #include <linux/gpio.h>
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
