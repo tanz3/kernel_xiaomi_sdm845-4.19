95af84918d2be115460e8dcc6bd40bea12a63699
tanz3 <3222839916@qq.com>
2023-04-16 11:57:39 +0800

    techpack: audio: Import xiaomi modifications
    Signed-off-by: tanz3 <3222839916@qq.com>

--- techpack/audio/include/dsp/q6afe-v2.h
+++ techpack/audio/include/dsp/q6afe-v2.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2019 XiaoMi, Inc.
  */
 #ifndef __Q6AFE_V2_H__
 #define __Q6AFE_V2_H__
@@ -295,6 +296,7 @@ enum {
 	IDX_RT_PROXY_PORT_002_TX,
 	/* IDX 212 */
 	IDX_HDMI_RX_MS,
+	IDX_AFE_PORT_ID_PSEUDOPORT_01,
 	AFE_MAX_PORTS
 };
 
